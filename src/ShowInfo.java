98969397510b51d69305267bc0003ea16b2eb8d7
hallwayskiing <541814852@qq.com>
2022-09-09 20:00:14 +0800

    1

---
+++ src/ShowInfo.java
@@ -0,0 +1,9 @@
+public class ShowInfo {
+    public static void main(String[] args) {
+        System.out.println("Name:聂宇翔\n" +
+                           "Sex:Male\n" +
+                           "Home:Nanchang\n" +
+                           "Address:C4"
+        );
+    }
+}
