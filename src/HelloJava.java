52ce915f1f1fed6edca0eaa4351d47a31d719d8d
hallwayskiing <541814852@qq.com>
2022-09-15 15:19:43 +0800

    NEW

---
+++ src/HelloJava.java
@@ -0,0 +1,7 @@
+public class HelloJava {
+
+    public static void main(String[] args) {
+        String s="HelloJava!";//声明字符串s
+        System.out.println(s);//打印字符串s
+    }
+}
---
+++ src/SumofOddNumber.java
@@ -0,0 +1,13 @@
+public class SumofOddNumber {
+
+
+    public static void main(String[] args) {
+        int sum=0;
+        for(int i=1;i<=99;i+=2)
+        {
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+
+}
