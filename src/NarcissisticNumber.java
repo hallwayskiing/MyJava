e50e0a130b78b4e2661fd18f0a580777e52ed02c
hallwayskiing <541814852@qq.com>
2022-09-16 10:17:56 +0800

    NEW

--- src/NarcissisticNumber.java
+++ src/NarcissisticNumber.java
@@ -2,7 +2,11 @@ public class NarcissisticNumber {
     public static void main(String[] args) {
         for(int i=100;i<1000;i++)
         {
-            if（i
+            int hundred=i/100;
+            int ten=(i-hundred*100)/10;
+            int single=i-hundred*100-ten*10;
+            if(i==hundred*hundred*hundred+ten*ten*ten+single*single*single)
+                System.out.println(i);
         }
     }
 }
