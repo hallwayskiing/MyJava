5539a39f8728f5834a09f58fea419d9bc8054d0b
hallwayskiing <541814852@qq.com>
2022-09-21 15:44:02 +0800

    NEW

--- src/NumList.java
+++ src/NumList.java
@@ -1,10 +1,23 @@
 public class NumList {
     public int[]nums;
+    public static void main(String[] args) {
+        NumList myList=new NumList();
+        myList.nums=new int[]{44,25,83,32,4,57,62,18,96,72};
+
+        myList.sortList();
+
+        myList.insertNumber(1000);
+
+        for (int num : myList.nums)
+            System.out.println(num);
+    }
+
     public void insertNumber(int number)
     {
         int []oldNums=nums.clone();
         nums=new int[nums.length+1];
         boolean isFirst=true;
+
         for (int i=0,j=0;i< oldNums.length;i++,j++)
         {
             if(number<=oldNums[i]&&isFirst)
@@ -15,24 +28,22 @@ public class NumList {
             }
             nums[j]=oldNums[i];
         }
+
+        if(isFirst)
+            nums[nums.length-1]=number;
     }
-    public static void main(String[] args) {
-        NumList myList=new NumList();
-        myList.nums=new int[]{44,25,83,32,4,57,62,18,96,72};
-        for(int i=0;i< myList.nums.length;i++)
-            for(int j=i+1;j< myList.nums.length;j++)
-        {
-            if(myList.nums[j]<myList.nums[i])
+
+    public void sortList()
+    {
+        for(int i=0;i< nums.length;i++)
+            for(int j=i+1;j< nums.length;j++)
             {
-                int temp=myList.nums[j];
-                myList.nums[j]=myList.nums[i];
-                myList.nums[i]=temp;
+                if(nums[j]<nums[i])
+                {
+                    int temp=nums[j];
+                    nums[j]=nums[i];
+                    nums[i]=temp;
+                }
             }
-        }
-        myList.insertNumber(1);
-        for (int num : myList.nums)
-        {
-            System.out.println(num);
-        }
     }
 }
