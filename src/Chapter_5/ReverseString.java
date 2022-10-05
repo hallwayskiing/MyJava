e66da15006c42b8609ad0fcb6a6008223a4432a0
hallwayskiing <541814852@qq.com>
2022-10-05 14:25:02 +0800

    NEW

--- src/Chapter_5/ReverseString.java
+++ src/Chapter_5/ReverseString.java
@@ -1,26 +1,17 @@
 package Chapter_5;
 
-import java.util.Stack;
 
 public class ReverseString {
     public static boolean isReverseString(String s) {
-        Stack<Character> reversed = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            reversed.push(ch);
-        }
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (!reversed.pop().equals(ch))
+        for (int i = 0; i < s.length()/2; i++){
+            if (s.charAt(i)!=s.charAt(s.length()-i-1))
                 return false;
         }
-
         return true;
     }
 
     public static void main(String[] args) {
-        String s="eeabcdcbaee";
+        String s="eeabwwcdcwwbaee";
         if(isReverseString(s)) System.out.println("是回文字符串");
         else System.out.println("不是回文字符串");
     }
