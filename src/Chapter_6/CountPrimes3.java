831b8eb7d292603184878943226b8510e6199046
hallwayskiing <541814852@qq.com>
2022-10-07 20:05:50 +0800

    NEW

---
+++ src/Chapter_6/CountPrimes3.java
@@ -0,0 +1,32 @@
+package Chapter_6;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CountPrimes3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> res = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input a number:");
+        int n = input.nextInt();
+        if (n <= 2) {
+            System.out.println("Not a primie");
+            return;
+        } else {
+            for (int i = 2; i < n; i++) {
+                boolean flag = true;
+                for (int j : res) {
+                    if (i % j == 0) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    res.add(i);
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println("\n" + n + "之内有" + res.size() + "个质数");
+        }
+    }
+}
